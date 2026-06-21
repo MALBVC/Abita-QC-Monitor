5cd7595d429eab076ad9d1ea6bda728b8593d258
Levi <malbvcllc@gmail.com>
2026-06-20 20:05:49 -0500

    Publish dashboard update

--- qc-data.js
+++ qc-data.js
@@ -8745,7 +8745,7 @@ const PIPELINE_META = {
     "TBD"
   ],
   "lastRun": "2026-06-20",
-  "lastSync": "Jun 20, 2026 \u00b7 4:05 PM",
+  "lastSync": "Jun 20, 2026 \u00b7 8:05 PM",
   "dbPath": "data/qc_monitor.db",
   "apiBase": "/api"
 };
